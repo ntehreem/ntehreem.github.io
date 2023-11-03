fd375c4ae9c9207616e78177195065de575dfa73
ntehreem <147871916+ntehreem@users.noreply.github.com>
2023-11-03 21:44:15 +0100

    Second version - ASTRONOMY CORNER

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function validarForm() {
     alert("Hey there Astronomer");
-    //var nom = document.forms["formulari"]["nom"].value;
-    //var nom =document.getElementByName("nom")[0].value; //per name="nom"
-    var Nom = document.getElementById("nom").value; //per id="nom"
-    // var pass = document.getElementById("pass").value;
-    // var doc = document.getElementById("doc").value;
-    alert(Nom +" Please coNfirm to this contact form, to confirm select 'OK'");
+    var Nom = document.getElementById("nom").value;
+    alert(Nom + " Please confirm to this contact form, to confirm select 'OK'");
+    alert(Nom + " Your form has been sent correctly we'll shortly get in touch with you.");
     if (validaContra(pass) && validaDoc(doc)) {
         alert("Form sent correctly. Thankyou!! " + Nom);
         document.getElementById("formulari").submit();
